ac91c11bc0e0ea8ba72f7cc386f1e65fc16b268e
Todd Hendricks <hendricks.ta@gmail.com>
2017-12-12 16:57:18 -0800

    commit existing query to be modified

--- eligible_applicants.sql
+++ eligible_applicants.sql
@@ -5,3 +5,8 @@ DATEDIFF(MONTH,profiles.service_start, profiles.service_end) <= 180
 -- Active? 
 now() > profiles.service_end
 
+-- Existing query
+SELECT users.first_name, users.last_name, users.id 
+as user_id, profiles.id as profile_id, profiles.slug, users.email 
+FROM profiles LEFT JOIN users ON users.id = profiles.user_id 
+WHERE stage >= 9 ORDER BY profiles.updated_at DESC
